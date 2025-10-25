7206ed89020aa30500853a7d6fab66fc13fb87ce
LenaBraunschweig <lena.braunschweig@gmail.com>
2025-10-25 14:34:41 -0500

    added game method and fixed some bugs related to player lookup

--- game.py
+++ game.py
@@ -1,6 +1,12 @@
 import csv
 import pandas as pd
 
+''' to do:
+    -   fix the printing so there are extra lines between unrelated output
+    -   add some debugging for items that are inputs that aren't valid
+'''
+
+
 question_type = None
 partner = None
 winner = None
@@ -21,7 +27,7 @@ def startup(file_name):
     # create the people
     player_names = []
     for player_number in range(1, 6):
-        player_name = input(f"What is the name of player {player_number}")
+        player_name = input(f"Name of player {player_number}: ")
         player_names.append(player_name)
         current_player = Player(player_name, player_number)
         players.append(current_player)
@@ -31,10 +37,10 @@ def startup(file_name):
     with open(file, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(player_names)
-
+    
 def find_person(person):
     for player in players:
-        if ((player.number == int(person)) or (player.name == person)):
+        if ((player.name == person) or (person.isdigit() and (player.number == int(person)))):
             return player
     return None
         
@@ -55,9 +61,10 @@ def write_score(file_name):
 def display_scores(file_name):
     file = file_name + ".csv"
     df = pd.read_csv(file)
-    print(df)
+    print(df.to_string(index=False))
 
 def run_round():
+    global dealer_index
     print(f"{players[dealer_index].name} is the dealer for this round")
     question_type = input("Was this a regular round or a leaster round: ")
     if "regular" in question_type:
@@ -71,7 +78,7 @@ def run_round():
         dealer_index = 0
 
 def regular_round():
-    picker = input("Who was the winner?")
+    picker = input("Who was the winner: ")
     picker_person = find_person(picker)
     picker_person.picker = True
     def picker_wins(partner_yesno, ranking):
@@ -104,23 +111,23 @@ def regular_round():
                 if ((player.picker == False) and (player.partner == False)):
                     player.score += rank[ranking]
     
-    partner_choice = input("Was there a partner, yes or no?")
+    partner_choice = input("Was there a partner, yes or no: ")
     if partner_choice == "yes":
-        partner = input("Which player was the partner?")
+        partner = input("Which player was the partner: ")
         partner_person = find_person(partner)
         partner_person.partner = True
     else:
         partner_person = None
     
-    win_lose = input("Did the picker win?")
-    ranking = input("Was it a regular round, no schnitz, or no tricker")
+    win_lose = input("Did the picker win: ")
+    ranking = input("Was it a regular round, no schnitz, or no tricker: ")
     if win_lose == "yes":
         picker_wins(partner_person, ranking)
     else:
         picker_loses(partner_person, ranking)
 
 def leaster_round():
-    winner = input("Who was the winner? (type 'tie' for a tie)")
+    winner = input("Who was the winner? (type 'tie' for a tie): ")
     if winner != "tie":
         winner_choice = find_person(winner)
         winner_choice.score += 20
@@ -129,4 +136,12 @@ def leaster_round():
                 player.score -= 5
 
 def game():
-    pass
+    input_file = input("File name- ") # want to change later to match the time and date
+    startup(input_file)
+    while True: # need to change until someone reaches either 100 points or -100
+        run_round()
+        write_score(input_file)
+        display_scores(input_file)
+        reset_round()
+
+game()
